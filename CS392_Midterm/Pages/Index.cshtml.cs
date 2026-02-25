7becb7d020fa0107aafc49c552b1442d25c5aa4a
bayleyEddy <bayley019@outlook.com>
2026-02-24 23:42:40 -0500

    Add project files.

---
+++ CS392_Midterm/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CS392_Midterm.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
